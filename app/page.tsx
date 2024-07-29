94e7307935f710f3a9ca0ef6ddfa314cb54f2f48
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-07-29 18:38:32 +0300

    change color fo car

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-'use client';
-import { OrbitControls, Environment } from '@react-three/drei';
-import { TeslaThree } from '@/component/teslathree';
-import { Canvas } from '@react-three/fiber';
+"use client";
+import { OrbitControls, Environment } from "@react-three/drei";
+import { TeslaThree } from "@/component/teslathree";
+import { Canvas } from "@react-three/fiber";
 export default function Home() {
   return (
     <>
-      <div style={{ backgroundColor: 'white', width: '100%', height: '100vh' }}>
+      <div style={{ backgroundColor: "white", width: "100%", height: "100vh" }}>
         <Canvas>
           <TeslaThree />
-          <OrbitControls minDistance={25} maxDistance={50} />
+          <OrbitControls minDistance={15} maxDistance={50} />
           <Environment preset="sunset" />
         </Canvas>
       </div>
--- component/teslathree.jsx
+++ component/teslathree.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import { useGLTF } from '@react-three/drei';
+"use client";
+import React, { useRef, useEffect } from "react";
+import { useGLTF } from "@react-three/drei";
 
 export function TeslaThree(props) {
-  const model = useGLTF('/scene.glb');
+  const model = useGLTF("/scene.glb");
+  const carMaterial = useRef();
+
+  // Function to change the color of the car paint
+  const changeCarPaintColor = () => {
+    if (carMaterial.current) {
+      console.log("I am called");
+      carMaterial.current.color.set("yellow"); // Change 'red' to your desired color
+    }
+  };
+  // Traverse the model and find the car paint material
+  const traverseMaterials = (object) => {
+    object.traverse((node) => {
+      if (node.isMesh) {
+        const materials = Array.isArray(node.material)
+          ? node.material
+          : [node.material];
+        materials.forEach((material) => {
+          if (material.name.includes("carpaint")) {
+            carMaterial.current = material;
+          }
+        });
+      }
+    });
+  };
+
+  // Call the function to traverse materials after the model is loaded
+  useEffect(() => {
+    traverseMaterials(model.scene);
+    changeCarPaintColor();
+  }, [model.scene]);
+
   return (
-    <mesh>
-      <primitive object={model.scene} />
-    </mesh>
+    <>
+      <mesh>
+        <primitive object={model.scene} />
+      </mesh>
+    </>
   );
 }
