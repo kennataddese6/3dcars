f5a72d41793c0062a81ffa7cb2d2ab9d088467da
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-07-30 11:25:26 +0300

    make users choose from a color palette

--- app/page.tsx
+++ app/page.tsx
@@ -4,30 +4,28 @@ import { TeslaThree } from "@/component/teslathree";
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
 export default function Home() {
-  const [color, setColor] = useState("purple");
+  const [color, setColor] = useState("#800080");
   return (
     <>
       <div className="visualContainer">
+        <div className="colorChoiceContainer">
+          <label className="center-text bold-text">
+            Choose your color here
+          </label>
+          <input
+            type="color"
+            id="favcolor"
+            name="favcolor"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+        </div>
         <div className="modelContainer">
           <Canvas>
             <TeslaThree color={color} />
             <OrbitControls minDistance={15} maxDistance={50} />
             <Environment preset="sunset" />
           </Canvas>
-        </div>
-        <div className="colorChoiceContainer">
-          <h1 className="center-text">Choose a color below</h1>
-          <ul>
-            <li style={{ color: "green" }} onClick={() => setColor("green")}>
-              Green
-            </li>
-            <li style={{ color: "yellow" }} onClick={() => setColor("yellow")}>
-              Yellow
-            </li>
-            <li style={{ color: "red" }} onClick={() => setColor("red")}>
-              Red
-            </li>
-          </ul>
         </div>
       </div>
     </>
