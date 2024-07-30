391787b1831d8f00f4f8a2adc4eb32f4af9c3b38
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-07-30 10:41:30 +0300

    add containers to visula

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,22 @@ import { Canvas } from "@react-three/fiber";
 export default function Home() {
   return (
     <>
-      <div style={{ backgroundColor: "white", width: "100%", height: "100vh" }}>
-        <Canvas>
-          <TeslaThree />
-          <OrbitControls minDistance={15} maxDistance={50} />
-          <Environment preset="sunset" />
-        </Canvas>
+      <div className="visualContainer">
+        <div className="modelContainer">
+          <Canvas>
+            <TeslaThree />
+            <OrbitControls minDistance={15} maxDistance={50} />
+            <Environment preset="sunset" />
+          </Canvas>
+        </div>
+        <div className="colorChoiceContainer">
+          <h1 className="center-text">Choose a color below</h1>
+          <ul>
+            <li style={{ color: "green" }}>Green</li>
+            <li style={{ color: "yellow" }}>Yellow</li>
+            <li style={{ color: "red" }}>Red</li>
+          </ul>
+        </div>
       </div>
     </>
   );
