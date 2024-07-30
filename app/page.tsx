baa0cc62a0c94f002de9bdb9bb9a666d98caf393
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-07-30 12:02:53 +0300

    add my own color picker

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,18 @@ import { TeslaThree } from "@/component/teslathree";
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
 export default function Home() {
-  const [color, setColor] = useState("#800080");
+  const [color, setColor] = useState("#000");
   return (
     <>
       <div className="visualContainer">
-        <input
-          type="color"
-          id="grad-input-4"
-          className="color-input"
-          name="favcolor"
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-        />
-        <div
-          id="custom-input-4"
-          className="custom"
-          style={{ backgroundColor: color }}
-        />
+        <div className="colorpicker" style={{ backgroundColor: color }}>
+          <input
+            type="color"
+            className="colorinput"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+        </div>
         <div className="modelContainer">
           <Canvas>
             <TeslaThree color={color} />
